5ffbf6b7a7882672dc6a7ad1f986686af11783bc
Goutham <gouthamrpapro@gmail.com>
2021-12-28 11:02:25 +0530

    formed a connection between video streaming service and aws storage service using nodejs built in http library

--- videoStreaming/index.js
+++ videoStreaming/index.js
@@ -1,12 +1,16 @@
 
 //****** Constants for hello world **********
 const express = require('express') //Loads the Express library for use in our code
+const http = require("http")
 const app = express() // Creates an instance of an Express app 
-const port = 3000 // Our HTTP server will listen on port 3000. 
+const port = 4001 // Our HTTP server will listen on port 3000.
+
+const VIDEO_STORAGE_HOST = "127.0.0.1"
+const VIDEO_STORAGE_PORT = 4000
 //**********************************************
 
 //****** Constants for video stream **********
-const fs =  require("fs") //Loads the (built-in) fs library so we can use the Node.js filesystem API.
+// const fs =  require("fs") //Loads the (built-in) fs library so we can use the Node.js filesystem API.
 //**********************************************
 
 
@@ -24,28 +28,35 @@ const fs =  require("fs") //Loads the (built-in) fs library so we can use the No
 
 //***************** HTTP server which will responsd back with video stream ***************************************************
 
-app.get("/video", (req, res) => { //Defines the HTTP route for streaming video. This is a REST API for streaming video!
+console.log(`Forwarding video requests to ${VIDEO_STORAGE_HOST}:${VIDEO_STORAGE_PORT}.`);
 
-
-  const path = "./video/F1.mp4"; //The path of the video file that we’ll stream to the browser
-  fs.stat(path, (err, stats) => { //Handles any errors that may occur
-      if (err) {
-          console.error("An error occurred ");
-          res.sendStatus(500);
-          return;
-      }
-
-      res.writeHead(200, { //Retrieves the video file size. We’ll encode this in the HTTP header as a response to the web browser. 
-          "Content-Length": stats.size, //Sends a response header to the web browser, including the content length and mime type
-          "Content-Type": "video/mp4",
-      });
-      fs.createReadStream(path).pipe(res); //Streams the video to the web browser. Yes it’s this simple!
-  });
+//
+// Registers a HTTP GET route for video streaming.
+//
+app.get("/video", (req, res) => {
+    const forwardRequest = http.request( // Forward the request to the video storage microservice.
+        {
+            host: VIDEO_STORAGE_HOST,
+            port: VIDEO_STORAGE_PORT,
+            path: '/video?path=file_example_MP4_480_1_5MG.mp4', // Video path is hard-coded for the moment.
+            method: 'GET',
+            headers: req.headers
+        }, 
+        forwardResponse => {
+            res.writeHeader(forwardResponse.statusCode, forwardResponse.headers);
+            forwardResponse.pipe(res);
+        }
+    );
+    
+    req.pipe(forwardRequest);
 });
 
+//
+// Starts the HTTP server.
+//
 
 app.listen(port, () => {
-  console.log(`Microservice listening on port ${port}, point your browser at http://localhost:3000/video`);
+  console.log(`Microservice listening on port ${port}, point your browser at http://localhost:${port}/video`);
 });
 
 //**********************************************************************************************************************
